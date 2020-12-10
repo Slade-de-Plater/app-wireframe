d2112111935820debfb33ba69292c026335fc37f
jotham-dorairaj <jothamdorairaj@gmail.com>
2020-12-10 14:22:49 +1100

    val order

--- index.js
+++ index.js
@@ -10,31 +10,17 @@ newTaskForm.addEventListener('submit', (event) => {
     const taskName = document.querySelector('#taskName');
     const description = document.querySelector('#description');
     const assignedTo = document.querySelector('#assignedTo');
+    const dueDate = document.querySelector('#date');
    
     const errorMessage = document.querySelector('#alertMessage');
     /*
         Validation code here
-    */  const name = taskName.value;
+    */  
+    const name = taskName.value;
     const descriptionVal = description.value;
     const assignedToVal = assignedTo.value;
+    const dueDateVal = dueDate.value;
 
-    if(!validFormFieldInput(descriptionVal)){
-        errorMessage.innerHTML += "Invalid name input";
-        errorMessage.style.display = "block"
-    }else{
-        errorMessage.style.display = "none"
-    }
-
-    if(!validFormFieldInput(assignedToVal)){
-        errorMessage.innerHTML += "Invalid name input";
-        errorMessage.style.display = "block"
-    }else{
-        errorMessage.style.display = "none"
-    }
-
-    // Get the values of the inputs
-  
-  
     if(!validFormFieldInput(name)){
         errorMessage.innerHTML += "Invalid name input";
         errorMessage.style.display = "block"
@@ -42,6 +28,33 @@ newTaskForm.addEventListener('submit', (event) => {
         errorMessage.style.display = "none"
     }
 
+    if(!validFormFieldInput(assignedToVal)){
+        errorMessage.innerHTML += "Invalid assignee";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+
+
+    if(!validFormFieldInput(descriptionVal)){
+        errorMessage.innerHTML += "Invalid description";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+
+ if(!validFormFieldInput(descriptionVal)){
+        errorMessage.innerHTML += "Invalid description";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+  
+    // Get the values of the inputs
+  
+
+
+  
     
 
 });
