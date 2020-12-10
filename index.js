eb5c31c2f5e3c319f8b1b7e0bc986f6381dd9d65
jotham-dorairaj <jothamdorairaj@gmail.com>
2020-12-10 14:05:58 +1100

    validation

---
+++ index.js
@@ -0,0 +1,51 @@
+    // Select the New Task Form
+const newTaskForm = document.querySelector('#userForm');
+
+    // Add an 'onsubmit' event listener
+newTaskForm.addEventListener('submit', (event) => {
+    // Prevent default action
+    event.preventDefault();
+
+    // Select the inputs
+    const taskName = document.querySelector('#taskName');
+    const description = document.querySelector('#description');
+    const assignedTo = document.querySelector('#assignedTo');
+   
+    const errorMessage = document.querySelector('#alertMessage');
+    /*
+        Validation code here
+    */  const name = taskName.value;
+    const descriptionVal = description.value;
+    const assignedToVal = assignedTo.value;
+
+    if(!validFormFieldInput(descriptionVal)){
+        errorMessage.innerHTML += "Invalid name input";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+
+    if(!validFormFieldInput(assignedToVal)){
+        errorMessage.innerHTML += "Invalid name input";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+
+    // Get the values of the inputs
+  
+  
+    if(!validFormFieldInput(name)){
+        errorMessage.innerHTML += "Invalid name input";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+
+    
+
+});
+
+function validFormFieldInput(data){
+    return data !== null && data !== '';
+}
